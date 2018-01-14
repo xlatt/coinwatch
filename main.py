5fca23693b882d995922ab7a85f74c2c040a52da
xlatt <burianpato@gmail.com>
2018-01-14 12:21:50 +0100

    add query builder

--- main.py
+++ main.py
@@ -1,62 +1,84 @@
 #!/bin/env python
 import ssl
 import socket
+from enum import Enum
 
 main_coins  = ["BTC", "btc", "ETH", "eth"]
 alt_coins   = ["HPB", "hpb"]
 
+class QueryType(Enum):
+    PRICE_TICKER = 1
+    MARKET_DEPTH = 2
+
+
+class QueryBuilder():
+    def build(self, market, qtype, *arg):
+        if qtype == QueryType.PRICE_TICKER:
+            return "GET /api/v1/ticker?symbol="+arg[0]+"_"+arg[1]+" HTTP/1.1\r\nHost: "+market+"\r\nConnection: keep-alive\r\n\r\n"
+
 class Market:
-    tcp_socket  = 0
-    ssl_wraper  = 0
-    address     = ""
-    api_key     = ""
-    secret_key  = ""
+    tcp_socket      = 0
+    ssl_wraper      = 0
+    address         = ""
+    api_key         = ""
+    secret_key      = ""
+    market_query    = None
+
 
     def __init__(self, address, api_key, secret_key):
         self.address = address
         self.api_key = api_key
         self.secret_key = secret_key
+        self.market_query = QueryBuilder()
 
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_socket.connect((self.address, 443))
         self.ssl_wraper = ssl.wrap_socket(self.tcp_socket, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
 
+
     def __del__(self):
         self.ssl_wraper.close()
 
-    # Relative to USD
-    def rt_usd(self, coin):
-        self.ssl_wraper.sendall("GET /api/v1/ticker?symbol="+coin+"_usd HTTP/1.1\r\nHost: "+self.address+"\r\nConnection: keep-alive\r\n\r\n")
+
+    def _coin_info(self, crncy1, crncy2):
+        self.ssl_wraper.sendall("GET /api/v1/ticker?symbol="+crncy1+"_"+crncy2+" HTTP/1.1\r\nHost: "+self.address+"\r\nConnection: keep-alive\r\n\r\n")
 
         resp = self.ssl_wraper.recv(4096)
         if not resp:
-            print("Failed to obtain info about "+coin)
+            print("Failed to obtain info about "+crncy1+" relative to "+crncy2)
             self.ssl_wraper.close()
             return
 
         print resp
 
-    # Relative to BTC
-    def rt_btc(self, coin):
-        self.ssl_wraper.sendall("GET /api/v1/ticker?symbol="+coin+"_btc HTTP/1.1\r\nHost: "+self.address+"\r\nConnection: keep-alive\r\n\r\n")
+
+    def query_market(self, query):
+        self.ssl_wraper.sendall(query)
 
         resp = self.ssl_wraper.recv(4096)
         if not resp:
-            print("Failed to obtain info about "+coin)
+            print("Market "+self.address+" did not respod for query: "+query)
             self.ssl_wraper.close()
             return
-
+        # todo strip http header and return
         print resp
+
 
     def coin_info(self, coin):
         if coin in main_coins:
-            self.rt_usd(coin)
+            q = self.market_query.build(self.address, QueryType.PRICE_TICKER, coin, "usd")
+            self.query_market(q)
         elif coin in alt_coins:
-            self.rt_btc(coin)
+            q = self.market_query.build(self.address, QueryType.PRICE_TICKER, coin, "btc")
+            self.query_market(q)
 
     def last_price(self, coin_info):
         raise "last price unimplemented"
 
+
+#######################
+#        MAIN         #
+#######################
 m = Market("api.allcoin.com", "", "")
 m.coin_info("btc")
 m.coin_info("hpb")
